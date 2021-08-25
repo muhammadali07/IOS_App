eb958bec776b47f23bfcc3137238d208b61f8c0a
Muh Ali Bakhtiar <muhalibakhtiar@gmail.com>
2021-08-25 23:13:31 +0700

    Create I Am Rich App
    Create I Am Rich App for lesson of Udemy Online Course by Dr Angela yu

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by PyDev on 19/08/21.
+//  Copyright © 2021 PyDev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
